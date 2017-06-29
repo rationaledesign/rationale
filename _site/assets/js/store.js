d6cd0ebfdc7b3243c5d28626ba51417ce54a6395
joshuarule <joshua@joshuarule.com>
2017-06-29 09:50:58 -0700

    remove _site

--- _site/assets/js/store.js
+++
@@ -1,9 +0,0 @@
-// archive and store gallery
-
-$(document).ready(function() {
-  $(".product-thumbnail").click(function(){
-    $(".active").removeClass("active");
-    $(this).find(".thumb-wrap").addClass("active");
-    $(".product-img .lazy" ).replaceWith($(".lazy", this).clone());
-  });
-});
