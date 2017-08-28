bc382debca8482d927a5f01713e0e86af5149534
victorfleur <victorfleur@hotmail.it>
2017-08-28 22:03:35 +0200

    updated jquery, installed owlcarousel

--- _assets/js/image-gallery.js
+++ _assets/js/image-gallery.js
@@ -1,9 +1,10 @@
 // archive and store gallery
 
 $(document).ready(function() {
-  $(".gallery-thumbnail").click(function(){
+  /*$(".gallery-thumbnail").click(function(){
     $(".active").removeClass("active");
     $(this).addClass("active");
     $(".gallery-img .lazy" ).replaceWith($(".lazy", this).clone());
-  });
+  });*/
+  $(".owl-carousel").owlCarousel();
 });
