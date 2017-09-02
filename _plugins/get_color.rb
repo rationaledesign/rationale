251fae200be85b259623e23492f8d48869838c67
victorfleur <victorfleur@hotmail.it>
2017-09-02 22:35:37 +0200

    comments

--- _plugins/get_color.rb
+++ _plugins/get_color.rb
@@ -17,25 +17,30 @@ class GetColorTag < Liquid::Tag
         filename = "cached_colors.csv"
 
         if var.nil? || var.empty?
-            "transparent"
+            # if image is empty
+            return "transparent"
         else
             flag = 0
 
+            # if cached file exists, check inside to see if the name matches
             if File.file?(filename)
                 CSV.foreach(filename) do |row|
                     if row[0] == var && flag == 0
                         flag = 1
+                        # return the color if there's a match
                         return row[1]
                     end
                 end
             else 
+            #else create a new file
                 CSV.open(filename, "wb")
             end
 
+            # if nothing was found, get the color from imgix 
             if flag == 0
                 uri = URI.parse('https://rationale-design.imgix.net' + var + '?palette=json&colors=2')
                 
-                # get json
+                # get resource with 5s timeout
                 begin
                     http = Net::HTTP.new(uri.host, uri.port)
                     http.use_ssl = true if uri.scheme == 'https'
@@ -45,13 +50,17 @@ class GetColorTag < Liquid::Tag
                         request = Net::HTTP::Get.new(uri.request_uri)
                         resp = http.request(request)
 
+                        # parse json
+                        # (I'm getting the second main color out of 2 because I found it was more pleasant)
                         json = JSON.parse(resp.body)
                         color = json["colors"][1]["hex"]
 
+                        # cache to color to file
                         CSV.open(filename, "a+b") do |csv|
                             csv << [var, color.to_s]
                         end
                 
+                        # and return the hex color
                         return color.to_s
                     end
 
