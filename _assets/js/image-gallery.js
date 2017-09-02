905c5748dfe6f6211f89d14f11490d9dd083e4ad
victorfleur <victorfleur@hotmail.it>
2017-09-02 22:30:54 +0200

    spacing fix and gallery click

--- _assets/js/image-gallery.js
+++ _assets/js/image-gallery.js
@@ -13,6 +13,14 @@ $(document).ready(function() {
     var target = $(this).data("target") - 1;
     owl.trigger('to.owl.carousel', [target, 300]);
   });
+  $(".owl-carousel").click(function(e){
+    var pWidth = $(this).innerWidth();
+    var pOffset = $(this).offset(); 
+    var x = e.pageX - pOffset.left;
+
+    if(pWidth/2 > x) owl.trigger('prev.owl.carousel', [300]);
+    else owl.trigger('next.owl.carousel', [300]);
+  });
 
   // updates active item after movement
   owl.on('changed.owl.carousel', function(event) {
