1ee4c82faf7bc4d67792098d75410fefd627e64d
joshuarule <joshua@joshuarule.com>
2017-09-25 11:57:44 -0400

    adds masonry layout

--- _assets/js/grid.js
+++ _assets/js/grid.js
@@ -9,3 +9,64 @@ document.body.addEventListener("keyup", function(e){
   }
   map = {};
 })
+
+// masonry layout
+
+if (window.innerWidth > 559) {
+
+  var posts = document.querySelectorAll('.feed-posts');
+
+  posts.forEach(function(container, i){
+    var msnry = new Masonry( container, {
+      itemSelector: '.feed-post',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      transitionDuration: 0
+    });
+  })
+}
+
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function() {
+    if(!posts && window.innerWidth > 559) {
+      window.posts = document.querySelectorAll('.feed-posts');
+      posts.forEach(function(container, i){
+        var msnry = new Masonry( container, {
+          itemSelector: '.feed-post',
+          columnWidth: '.grid-sizer',
+          percentPosition: true,
+          transitionDuration: 0
+        });
+      })
+    } else if (posts && window.innerWidth < 559) {
+      posts.forEach(function(container){
+        var msnry = new Masonry( container, {
+          itemSelector: '.feed-post',
+          columnWidth: '.grid-sizer',
+          percentPosition: true,
+          transitionDuration: 0
+        });
+        msnry.destroy();
+      })
+      window.posts = null;
+    }
+});
