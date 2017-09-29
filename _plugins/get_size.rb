7bccab4bb8238b493250e7b2d6090161edcbc348
victorfleur <victorfleur@hotmail.it>
2017-09-29 23:15:53 +0200

    fix

--- _plugins/get_size.rb
+++ _plugins/get_size.rb
@@ -63,7 +63,7 @@ class GetSizeTag < Liquid::Tag
                             CSV.open(filename, "a+b") do |csv|
                                 csv << [var, printheight]
                             end
-                        else return res.class.name
+                        else return resp.class.name
                         end
                 
                         # and return the hex color
